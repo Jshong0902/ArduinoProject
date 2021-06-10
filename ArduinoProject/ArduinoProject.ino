47c466f000f920a25ebce94e97803577c6988b30
Jshong0902 <jshong0902@gmail.com>
2021-06-10 19:47:58 +0900

    블루투스 제어 약간 성공

--- ArduinoProject/ArduinoProject.ino
+++ ArduinoProject/ArduinoProject.ino
@@ -1,11 +1,11 @@
+//AT+ROLE=1
 #include <SoftwareSerial.h>
 #include <LiquidCrystal_I2C.h>
 
-#define fanpin1 7
-#define fanpin2 8
-#define BTtx 2
-#define BTrx 3
+#define f1 9
+#define f2 10
 #define LCD1 18
+
 #define LCD2 19
 SoftwareSerial BT(2, 3);
 
@@ -15,17 +15,19 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
   BT.begin(9600);
   Serial.begin(9600);
-  pinMode(fanpin1, OUTPUT);
-  pinMode(fanpin2, OUTPUT);
+  pinMode(f1, OUTPUT);
+  pinMode(f2, OUTPUT);
   lcd.begin();
 }
 
 void loop() {
-  if (BT.available()) {
-    Serial.write(BT.read());
+  if (BT.available() > 0) {
+    data = BT.read();
   }
-  if (BT.available()) {
-    BT.write(Serial.read());
+  if (data=='a'){
+    digitalWrite(f1, 1);
   }
-
+  if (data=='d'){
+    digitalWrite(f1, 0);
+  }
 }
